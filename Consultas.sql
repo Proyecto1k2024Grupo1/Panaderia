10152ce82d87e047e5a1e84d973c2531bbe447cc
Jessica Brotons Maciá <jesbromac@alu.edu.gva.es>
2025-01-06 19:56:42 +0100

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -11,3 +11,28 @@ where co.fecha = '2024-01-01';
 select e.nombre, r.dni 
 from repartidor r
 join empleado e ON r.dni = e.dni;
+
+
+-- 1. Lista el empleado más joven junto con su fecha de nacimiento.
+SELECT nombre, fnac
+FROM EMPLEADO
+ORDER BY fnac DESC
+LIMIT 1;
+
+-- 2. Lista los clientes los cuales su número de teléfono empiece por "1111".
+SELECT c.nombre, c.apellido1, c.apellido2, t.numTelefono
+FROM CLIENTE c
+INNER JOIN TELEFONO t ON c.idCliente = t.idCliente
+WHERE t.numTelefono LIKE '1111%'
+ORDER BY c.nombre;
+
+-- 3.Lista los productos cuyo precio no es compartido por ningún otro producto.
+SELECT p1.nombre, p1.precio
+FROM PRODUCTO p1
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM PRODUCTO p2
+    WHERE p1.precio = p2.precio
+      AND p1.codigo != p2.codigo
+)
+ORDER BY p1.precio;

092fd959281c56b8d22a7b2e3fba9137e555a954
Cachón <silcaclei@alu.edu.gva.es>
2025-04-04 19:13:56 +0200

    v5

--- MODELO CLASES JAVA/Proyecto_JAVA/src/DAO/CompraDAO.java
+++ MODELO CLASES JAVA/Proyecto_JAVA/src/DAO/CompraDAO.java
@@ -1,6 +1,11 @@
 package DAO;
 
-import java.sql.Connection;
+import Model.Compra;
+import Model.Dependiente;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CompraDAO {
     private static CompraDAO instance;
@@ -13,4 +18,96 @@ public class CompraDAO {
     private static final String UPDATE_QUERY_REPARTIDOR = "UPDATE COMPRA SET dniRepartidor = ?, fechaRepartido = CURDATE(), horaRepartidor = CURTIME() where numCompra = ? ";
     private static final String DELETE_QUERY = "DELETE FROM COMPRA WHERE numCompra = ?";
 
+    /**
+     * Método estático para obtener la única instancia de DependienteDAO.
+     * @return instancia única de DependienteDAO.
+     */
+    private CompraDAO(){
+        this.connection = DBConnection.getConnection();
+    }
+
+
+    public void insertCompra(Compra compra) throws SQLException {
+
+        try (PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+        ) {
+            statement.setInt(1, compra.getCliente().getIdCliente());
+            statement.executeUpdate();
+
+        }
+    }
+
+    /**
+     * Obtiene todos los dependientes almacenados en la base de datos.
+     * @return Lista de objetos dependientes.
+     * @throws SQLException Si ocurre un error en la base de datos.
+     */
+    public List<Compra> getAllCompra() throws SQLException {
+        List<Compra> Compras = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(SELECT_ALL_QUERY)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Compras.add(resultSetToCompra(resultSet));
+            }
+        }
+        return Compras;
+    }
+
+    /**
+     * Convierte un ResultSet en un objeto dependiente.
+     * @param resultSet Resultado de la consulta SQL.
+     * @return Objeto Dependiente con los datos del ResultSet.
+     * @throws SQLException Por si ocurre un error en la conversión.
+     */
+    private Compra resultSetToCompra(ResultSet resultSet) throws SQLException {
+        return new Compra(
+                resultSet.getInt("numCompra")
+        );
+    }
+
+    /**
+     * Actualiza los datos de un dependiente en la base de datos.
+     * @param dependiente dependiente con los datos actualizados.
+     * @throws SQLException Si ocurre un error en la base de datos.
+     */
+    public void updateDependiente(Dependiente dependiente) throws SQLException {
+        connection.setAutoCommit(false);
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY_SUPER);
+             PreparedStatement statement2 = connection.prepareStatement(UPDATE_QUERY)
+        ) {
+            statement.setDouble(1, dependiente.getSalario());
+            statement.setDate(2, Date.valueOf(dependiente.getFnac()));
+            statement.setString(3, dependiente.getNombre());
+            statement.setString(4, dependiente.getEncargado().getDni());
+            statement.setString(5, dependiente.getDni());
+            statement.executeUpdate();
+
+
+            statement2.setString(1,dependiente.getHorario());
+            statement2.setString(2, dependiente.getDni());
+            statement2.executeUpdate();
+
+            connection.commit();
+        }
+    }
+
+    /**
+     * Elimina un dependiente de la base de datos por su DNI.
+     * @param dni Identificador único del dependiente a eliminar.
+     * @throws SQLException Si ocurre un error en la base de datos.
+     */
+    public void deleteDependienteByDni(String dni) throws SQLException {
+        connection.setAutoCommit(false);
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_QUERY);
+             PreparedStatement statement2 = connection.prepareStatement(DELETE_QUERY_SUPER)
+        ) {
+            statement.setString(1, dni);
+            statement.executeUpdate();
+
+            statement2.setString(1, dni);
+            statement2.executeUpdate();
+
+            connection.commit();
+        }
+    }
 }
--- MODELO CLASES JAVA/Proyecto_JAVA/src/Model/Compra.java
+++ MODELO CLASES JAVA/Proyecto_JAVA/src/Model/Compra.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-class Compra {
+public class Compra {
     int numCompra;
     LocalDate fecha;
     Cliente cliente;
@@ -27,5 +27,90 @@ class Compra {
 
     void asociarCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+
+    public Compra(int numCompra) {
+        this.numCompra = numCompra;
+    }
+
+    public int getNumCompra() {
+        return numCompra;
+    }
+
+    public void setNumCompra(int numCompra) {
+        this.numCompra = numCompra;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Dependiente getDependiente() {
+        return dependiente;
+    }
+
+    public void setDependiente(Dependiente dependiente) {
+        this.dependiente = dependiente;
+    }
+
+    public double getDescuentoDependiente() {
+        return descuentoDependiente;
+    }
+
+    public void setDescuentoDependiente(double descuentoDependiente) {
+        this.descuentoDependiente = descuentoDependiente;
+    }
+
+    public LocalDate getFechaDependiente() {
+        return fechaDependiente;
+    }
+
+    public void setFechaDependiente(LocalDate fechaDependiente) {
+        this.fechaDependiente = fechaDependiente;
+    }
+
+    public Repartidor getRepartidor() {
+        return repartidor;
+    }
+
+    public void setRepartidor(Repartidor repartidor) {
+        this.repartidor = repartidor;
+    }
+
+    public LocalDate getFechaRepartidor() {
+        return fechaRepartidor;
+    }
+
+    public void setFechaRepartidor(LocalDate fechaRepartidor) {
+        this.fechaRepartidor = fechaRepartidor;
+    }
+
+    public LocalTime getHoraRepartidor() {
+        return horaRepartidor;
+    }
+
+    public void setHoraRepartidor(LocalTime horaRepartidor) {
+        this.horaRepartidor = horaRepartidor;
+    }
+
+    public List<LineaDeTicket> getLineas() {
+        return lineas;
+    }
+
+    public void setLineas(List<LineaDeTicket> lineas) {
+        this.lineas = lineas;
     }
 }
